8751aedb83cf94bdae5f218149d0fdef95328791
WulanR <wulanr161299@gmail.com>
2018-01-30 12:58:54 +0700

    Modul10

---
+++ src/JoptionPane.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Milo Nestlea
+ */
+class JoptionPane {
+
+    static void showMessageDialog(Object object, String pin_salah_Pastikan_PIN_yang_anda_masukkan, String error, int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
